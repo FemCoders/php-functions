01c3a733e704fd10f937119eef2cf909903819aa
MAlexGG <alexandragalarzag@gmail.com>
2023-11-01 18:07:00 +0100

    functions exercises with tests an oop

---
+++ src/Item.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+//Ejercicio 1 -> Crear una clase Item que tenga 3 propiedades: name, price y quantity cuyo valor será declarado al crear un objeto. Cada propiedad debe ser capaz de ser retornada con un método propio.
+//Exercise 1 -> Create an Item class that has 3 properties: name, price and quantity whose value will be declared when creating an object. Each property must be able to be returned with its own method.
+
+//Escribe tu código aquí...
